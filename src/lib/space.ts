39d573e05670234a354851293c329774ec544f1a
Lavender <hi@lavylavender.com>
2025-02-17 13:44:49 -0800

    update space

--- src/lib/space.ts
+++ src/lib/space.ts
@@ -2,11 +2,11 @@ import { S3Client } from "@aws-sdk/client-s3";
 import { env } from "./env";
 
 export const s3Client = new S3Client({
-    endpoint: "https://sfo2.digitaloceanspaces.com",
+    endpoint: env.R2_ENDPOINT,
     forcePathStyle: false,
     region: 'us-east-1',
     credentials: {
-        accessKeyId: env.DIGITALOCEAN_SPACEKEY,
-        secretAccessKey: env.DIGITALOCEAN_SPACEKEY_PRIV
+        accessKeyId: env.R2_ACCESSKEY,
+        secretAccessKey: env.R2_SECRETKEY,
     }
 })
