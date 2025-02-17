211570ce5f121b666174eb6a55beec6e833bcbaf
Lavender <hi@lavylavender.com>
2025-02-17 13:45:20 -0800

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -6,8 +6,9 @@ dotenv.config();
 const envSchema = z.object({
     DISCORD_TOKEN: z.string(),
     DATABASE_URL: z.string(),
-    DIGITALOCEAN_SPACEKEY: z.string(),
-    DIGITALOCEAN_SPACEKEY_PRIV: z.string(),
+    R2_ENDPOINT: z.string(),
+    R2_ACCESSKEY: z.string(),
+    R2_SECRETKEY: z.string(),
     CRONITOR_KEY: z.string()
 });
 
