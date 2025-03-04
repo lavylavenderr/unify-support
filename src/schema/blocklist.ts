3b050540b2f2a07adeed984653776c4c0793010c
Lavender <hi@lavylavender.com>
2025-03-03 20:56:52 -0800

    chpre: wipe for recode

--- src/schema/blocklist.ts
+++
@@ -1,9 +0,0 @@
-import { timestamp } from 'drizzle-orm/pg-core';
-import { text } from 'drizzle-orm/pg-core';
-import { pgTable, varchar } from 'drizzle-orm/pg-core';
-
-export const blocklist = pgTable('blocklist', {
-	userId: varchar().primaryKey().unique(),
-	reason: text(),
-	createdAt: timestamp().defaultNow()
-});
--- src/schema/snippets.ts
+++
@@ -1,9 +0,0 @@
-import { varchar, pgTable, text, integer } from 'drizzle-orm/pg-core';
-
-export const snippets = pgTable('snippets', {
-	id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    identifier: varchar().notNull(),
-    content: text().notNull()
-});
-
-export type snippetType = typeof snippets.$inferInsert
